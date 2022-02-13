e33af668865232958deaedb00ff13e3363e334ca
Raph221001 <c18359123@tudublin.ie>
2022-02-13 21:20:20 +0000

    popularity recommender created

--- FYP/main.py
+++ FYP/main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import numpy as np
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Creates a popularity recommender class
+class popular_recommender():
+    def __init__(self):
+        self.train_data = None
+        self.user_id = None
+        self.item_id = None
+        self.popular_recommendations = None
 
+        #Creates the popularity based recommender system model
+        def create(self, train_data, user_id, item_id):
+            self.train_data = train_data
+            self.user_id = user_id
+            self.item_id = item_id
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            #Get a count of user_ids for each unique song as a recommendation score
+            train_data_grouped = train_data.groupby([self.item_id]).agg({self.user_id: 'count'}).reset_index()
+            train_data_grouped.rename(columns={user_id: 'score'}, inplace=True)
 
+            #Songs are sorted based on the recommendation score
+            train_data_sorting = train_data_grouped.sorting_values(['score', self.item_id], ascending=[0, 1])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+            #Generates a recommendation rank based on the score
+            train_data_sorting['Rank'] = train_data_sorting['score'].rank(ascending=0, method='first')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            #Gets the top 10 recommendations
+            self.popular_recommendations = train_data_sorting.head(10)
+
+        # Popularity based recommender system is used to make recommendations
+        def recommend(self, user_id):
+            user_recommendations = self.popularity_recommendations
+
+            # Adds a user_id column which is where the recommendations are generated
+            user_recommendations['user_id'] = user_id
+
+            # Brings the user_id column to the start
+            cols = user_recommendations.columns.tolist()
+            cols = cols[-1:] + cols[:-1]
+            user_recommendations = user_recommendations[cols]
+
+            return user_recommendations
